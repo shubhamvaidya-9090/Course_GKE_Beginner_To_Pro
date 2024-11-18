f8a93b60787594937d7a961ed48f4bd79e6acd87
Shubham Vaidya <shubham@p3clap-f54mzc2.dir.svc.accenture.com>
2024-11-18 03:37:54 -0600

    GCP Workflow

--- Chapter_Two/Lecture_7_Lab/myapp/server.py
+++ Chapter_Two/Lecture_7_Lab/myapp/server.py
@@ -5,7 +5,7 @@ import tornado.web
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         title = "Brocker app"
-        bgcolor = "dodgergreen"
+        bgcolor = "dodgerred"
         self.render("template.html", title=title, bgcolor=bgcolor)
         print(self.request)
 
